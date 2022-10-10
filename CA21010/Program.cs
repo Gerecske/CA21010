98dc608224064b9254606d9725a1afb48a93b1a7
szilagyi.gergely <szilagyi.gergely@28-09>
2022-10-10 10:49:01 +0200

    Add project files.

---
+++ CA21010/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace CA21010
+{
+    public abstract class Jarmu
+    {
+        public int TankMerete { get; set; }
+
+        public float BenzinMeny { get; set; }
+        public int SzallSzemSzam { get; set; }
+
+        public abstract void Tankol(float benzin);
+    }
+
+    public class Gapkocsi : Jarmu
+    {
+        public override void Tankol(float benzin)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Gapkocsi()
+        {
+            TankMerete = 80;
+            SzallSzemSzam = 5;
+        }
+    }
+
+    public class Motorkerekpar : Jarmu
+    {
+        public override void Tankol(float benzin)
+        {
+            if (BenzinMeny + benzin <= TankMerete)
+            {
+                BenzinMeny += benzin;
+            }
+            else
+            {
+                Console.WriteLine($"Tank tele maradt még {BenzinMeny + benzin - TankMerete} liter benzin!");
+                BenzinMeny = TankMerete;
+            }
+        }
+
+        public Motorkerekpar()
+        {
+            TankMerete = 15;
+            SzallSzemSzam = 2;
+        }
+    }
+
+    public class Tesla : Jarmu
+    {
+        public override void Tankol(float benzin)
+        {
+            Console.WriteLine($"nem kell a {benzin} liter benzin,\nDugd be a konekorba");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Jarmu g1 = new Gapkocsi();
+            Jarmu g2 = new Motorkerekpar();
+        }
+    }
+}
